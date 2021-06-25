43a5f78672f131e8dd829f9ca8526de4840d2652
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-06-25 22:16:18 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,27 +9,27 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
-async def on_ready():  
+async def on_ready():   #What the bot does when it starts up
   print("Ready")
-  await bot.change_presence(activity=discord.Game(name="Giving roles"))
+  await bot.change_presence(activity=discord.Game(name="Giving roles")) #He is permaplaying "giving roles with this"
 
 @bot.event
-async def on_raw_reaction_add(reaction):
-    msg = "858061816310792262"
-    id = str(reaction.message_id)
-    guild = bot.get_guild(reaction.guild_id)
-    usuario = guild.get_member(int(reaction.user_id))
+async def on_raw_reaction_add(reaction): #When someone reacts to some message this triggers
+    msg = "858061816310792262" #id of the message meant for react
+    id = str(reaction.message_id) #converts the id of the message who triggered this in a string
+    guild = bot.get_guild(reaction.guild_id) #know the id of the server
+    usuario = guild.get_member(int(reaction.user_id)) #know the id of who reacted
 
 
-    if msg == id:
-        print("si")
-        reaccion = str(reaction.emoji)
+    if msg == id: #if the id of the message who triggered this is the same of the message meant to be reacted
+        print("si") #comprobation
+        reaccion = str(reaction.emoji) #know the id of the reacion
         print(reaccion)
-        if "<:assettoicon:856622922453614612>" == reaccion:
+        if "<:assettoicon:856622922453614612>" == reaccion: #if the id of the reaction mathches the id of the reaction meant for this triggers
             print("si")
             
-            role = discord.utils.get(guild.roles, id=858064599243554826)
-            await usuario.add_roles(role)
+            role = discord.utils.get(guild.roles, id=858064599243554826) #know the id of the role wanted
+            await usuario.add_roles(role) #give the role to the user who reacted
         elif "<:acc:858073968379428868>" == reaccion:
             print("si2")
             
